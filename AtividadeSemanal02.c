237accf89098a6de753764a5cafed7e1bca3c2e6
André Morales <an.dre.m@outlook.com>
2023-04-28 08:39:31 -0300

    Commit inicial
    Upload dos primeiros arquivos do repo

---
+++ AtividadeSemanal02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("3.14159\n");
+    printf("%f\n",3.14159);
+    printf("%f\n",3*3.14159);
+    printf("%i\n",1+2+3+4+5);
+    printf("%i\n",9/(3*3));
+    return 0;
+}
---
+++ AtividadeSemanal03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int meuNumeroUSP = 123456789;
+    printf("meuNumeroUSP\n");
+    printf("%i\n",meuNumeroUSP);
+    printf("%i\n",meuNumeroUSP / 5);
+    printf("%i\n",meuNumeroUSP % 5);
+    return 0;
+}
