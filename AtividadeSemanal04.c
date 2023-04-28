29cf67eec60fd943cac76cbdf57f4b6867701d9e
André Morales <an.dre.m@outlook.com>
2023-04-28 09:15:44 -0300

    Atividade 4
    Subindo a solução da atividade 4, que calcula a área de algumas figuras e as imprime na tela.

--- AtividadeSemanal04.c
+++ AtividadeSemanal04.c
@@ -1,29 +1,40 @@
+/** Autor: André Morales
+  * No USP: 14558332
+  * Data: 28/04/2023
+  *
+  * # Exercício 4 de Introdução a Programação #
+  * Esse programa calcula algumas áreas de figuras
+  * geométricas, utilizando agora variáveis, parâmetros
+  * e funções para organizar o código.
+  */
+
 #include <stdio.h>
 
+/** Calcula a área de um trapézio com [(A + B) / 2 * H]
+  */
 double areaTrapezio(double baseMaior, double baseMenor, double altura){
-    double area = 0;
-
-    /* COMPLETAR */
-	
+    double area = (baseMenor + baseMaior) / 2 * altura;
     return area;
 }
 
+/** Calcula a área de um retângulo utilizando a fórmula 
+  * [B * H]. Também imprime na tela a área calculada.
+  */
 double areaRetangulo(double base, double altura){
-   double area = areaTrapezio(base,base,altura);
+   double area = areaTrapezio(base, base, altura);
+
    printf("Area do retangulo: %f\n", area);
    return area;
 }
 
+/** Calcula a área de um quadrado utilizando a fórmula [L^2] */
 double areaQuadrado(double lado){
-    double area = 0;
-
-    /* COMPLETAR */
-	
+    double area = lado * lado;
     return area;
 }
 
 int main(){
-    printf("%f\n",areaTrapezio(3,2,1));
-    areaRetangulo(11,27);
-    printf("%f\n",areaQuadrado(5));
+    printf("%f\n",areaTrapezio(3, 2, 1));
+    areaRetangulo(11, 27);
+    printf("%f\n", areaQuadrado(5));
 }
