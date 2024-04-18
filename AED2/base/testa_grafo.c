f1a4ce21f5cdbab80355cea5b99d7d5634b2329a
André Morales <an.dre.m@outlook.com>
2024-04-18 09:26:08 -0300

    feat: implementação inicial de um algoritmo de busca em profundidade

--- AED2/base/testa_grafo.c
+++ AED2/base/testa_grafo.c
@@ -10,6 +10,13 @@
 #include <stdio.h>
 #include <assert.h>
 
+typedef enum {
+	BUSCA_BRANCO, BUSCA_CINZA, BUSCA_PRETO
+} BuscaCor;
+
+void buscaProfundidade(Grafo* g);
+void visitaBP(Grafo* g, int v, int* tempo, BuscaCor cor[], int tempoDesc[], int tempoTerm[]);
+
 void leGrafo(Grafo* g, char* arquivo);
 void printAdjacentes(Grafo* g, int V);
 void pause();
@@ -21,11 +28,65 @@ void main() {
 
 	imprimeGrafo(g);
 
-	removeAresta(g, 1, 3, NULL);
-
-	imprimeGrafo(g);
+	buscaProfundidade(g);
 
 	liberaGrafo(g);
+}
+
+void buscaProfundidade(Grafo* g) {
+	int numVertices = obtemNrVertices(g);
+
+	// Inicialização das estruturas básicas
+	int tempo = 0;
+	BuscaCor cores[numVertices];
+	int tempoDesc[numVertices];
+	int tempoTerm[numVertices];
+
+	for (int i = 0; i < numVertices; i++) {
+		cores[i] = BUSCA_BRANCO;
+		tempoDesc[i] = 0;
+		tempoTerm[i] = 0;
+	}
+
+	// Visita o vértice inicial
+	int inicio = 0;
+	visitaBP(g, inicio, &tempo, cores, tempoDesc, tempoTerm);
+}
+
+// Visita um vértice em busca de profundidade.
+// V: O vértice a visitar
+// tempo: referência para o relógio geral do algoritmo, incrementado para cada evento de
+//        descoberta ou término
+// cor: referência para o arranjo de cores de todos os vértices
+// tempoDesc: referência para o tempo de descoberta de todos os vértices
+// tempoTerm: referência para o tempo de término de visita de todos os vértices
+void visitaBP(Grafo* g, int v, int* tempo, BuscaCor cor[], int tempoDesc[], int tempoTerm[]) {
+	// Se o vértice já foi descoberto, não faz nada.
+	if (cor[v] != BUSCA_BRANCO) return;
+
+	// Evento de descoberta: o vértice passa a ser cinza, incrementa o relógio global e
+	// registra o tempo de descoberta
+	cor[v] = BUSCA_CINZA;
+	tempoDesc[v] = ++(*tempo);
+
+	printf("%i: INI %i\n", v, tempoDesc[v]);
+
+	// Pega o primeiro vértice alcançável por V para iterar por todos os alcançáveis
+	Apontador ap = primeiroListaAdj(g, v);
+	while (ap) {
+		// Obtém o índice do vértice adjacente e invoca o algoritmo no adjacente
+		int adjacente = apontadorVertice(ap);
+		visitaBP(g, adjacente, tempo, cor, tempoDesc, tempoTerm);
+
+		// Avança para o próximo adjacente
+		ap = proxListaAdj(g, v, ap);
+	}
+
+	// Evento de término: troca a cor do vértice para preto e incrementa o relógio global
+	cor[v] = BUSCA_PRETO;
+	tempoTerm[v] = ++(*tempo);
+
+	printf("%i: FIM %i\n", v, tempoTerm[v]);
 }
 
 void leGrafo(Grafo* g, char* arquivo) {
